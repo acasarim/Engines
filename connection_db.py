8459a3ede3196da8da6f65aba1109b36f48f7386
Alexandre Casarim <85969067+acasarim@users.noreply.github.com>
2021-12-04 04:03:26 -0300

    Create connection_db.py

---
+++ connection_db.py
@@ -0,0 +1,27 @@
+import mysql.connector
+from mysql.connector import errorcode
+from mysql.connector.cursor import MySQLCursor
+
+class Database:
+
+    def __init__(self):
+        self.__db_con = mysql.connector.connect(host='',
+                                                user='',
+                                                password='',
+                                                database='')
+
+    def conectar(self) -> str:
+        try:
+            self.__db_con
+            print('CONEXÃO ABERTA')
+        except mysql.connector.Error as error:
+            if error.errno == errorcode.ER_BAD_DB_ERROR:
+                 print("DATABASE DOESN'T EXIST!")
+            elif error.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("USER NAME OR PASSWORD IS WRONG")
+            else:
+                print("error")
+              
+    def desconectar(self) -> str:
+        self.__db_con.close()
+        print('CONEXÃO FECHADA')
